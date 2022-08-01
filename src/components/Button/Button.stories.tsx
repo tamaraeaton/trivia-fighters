99d5f0004aac957eaa0c6917c08777eed288a908
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-08-01 13:20:03 -0700

    RAP404: - added other buttons

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -10,5 +10,17 @@ export const Correct = () => (
   <Button classType="btn--correct">Correct Button</Button>
 );
 
+export const Incorrect = () => (
+  <Button classType="btn--incorrect">Incorrect Button</Button>
+);
+
+export const Easy = () => <Button classType="btn--easy">Easy Button</Button>;
+
+export const Medium = () => (
+  <Button classType="btn--medium">Medium Button</Button>
+);
+
+export const Seth = () => <Button classType="btn--seth">Seth Button</Button>;
+
 // expectation to see 9 buttons in storybook
 // think about how to pass in sizing and icons
