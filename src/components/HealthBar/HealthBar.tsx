c668d50b2a360bf449003acbae4603324cd93e9e
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-08-04 09:39:53 -0700

    RAP403: - added css file - added 2nd bar

--- src/components/HealthBar/HealthBar.tsx
+++ src/components/HealthBar/HealthBar.tsx
@@ -3,7 +3,8 @@ import HealthFull from '../../assets/images/health-full.svg';
 import HealthLow from '../../assets/images/health-low.svg';
 
 const containerStyles = {
-  height: 20,
+  // display: 'flex',
+  height: 30,
   width: '100%',
   backgroundColor: '#e0e0de',
   borderRadius: 50,
@@ -32,16 +33,27 @@ const HealthBar: FunctionComponent<HealthBarProps> = ({
   const healthPercentage = Math.round((currentHealth / maxHealth) * 100);
 
   const fillerStyles = {
+    // display: 'flex',
     height: '100%',
     width: `${healthPercentage}%`,
     backgroundColor: 'green',
     borderRadius: 'inherit',
-    // textAlign: 'right',
+
+    // transform: 'translate(120px, 50%)',
+    // transform: 'rotateY(180deg)',
+    // justifyContent: 'left',
+    // alignItems: 'left',
+    // alignText: 'right',
+    // float: 'right',
   };
 
   if (healthPercentage < 50) {
     fillerStyles.backgroundColor = 'red';
   }
+
+  // if(isReversed === true) {
+
+  // }
 
   return (
     <>
@@ -52,7 +64,7 @@ const HealthBar: FunctionComponent<HealthBarProps> = ({
           ) : (
             <img src={HealthFull} />
           )}
-          <span style={labelStyles}>{`${healthPercentage}%`}</span>
+          <span style={labelStyles}>{`${healthPercentage}/100`}</span>
         </div>
       </div>
     </>
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,11 +3,21 @@ import Round from '../components/Round/Round';
 
 const Game: React.FunctionComponent = () => {
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
+      <HealthBar isReversed={true} maxHealth={150} currentHealth={100} />
       <Round />
-      <HealthBar isReversed={true} maxHealth={150} currentHealth={30} />
+      <HealthBar isReversed={true} maxHealth={150} currentHealth={60} />
     </div>
   );
 };
 
 export default Game;
+
+// transform: matrix(1, 2, 3, 4, 5, 6);
+// transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+// transform: translate(120px, 50%);
+// transform: scale(2, 0.5);
+// transform: rotate(0.5turn);
+// transform: skew(30deg, 20deg);
+// transform: scale(0.5) translate(-100%, -100%);
+// transform: perspective(17px);
