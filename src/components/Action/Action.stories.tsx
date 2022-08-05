3a3fe2d1668ab357b48745f7c5382550fa0160d4
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-08-05 13:29:25 -0700

    RAP403: - added Action component - added Action stories

---
+++ src/components/Action/Action.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory } from '@storybook/react';
+import Action, { ActionProps } from './Action';
+
+export default {
+  title: 'Action',
+  component: Action,
+};
+
+const Template: ComponentStory<typeof Action> = (args) => <Action {...args} />;
+
+export const AttackAction = Template.bind({});
+
+AttackAction.args = {
+  isReversed: false,
+  actionState: 'attack',
+  attackValue: 5,
+};
---
+++ src/components/Action/Action.tsx
@@ -0,0 +1,33 @@
+import { FunctionComponent } from 'react';
+import SwordIcon from '../../assets/images/sword.svg';
+import ShieldIcon from '../../assets/images/shield.svg';
+import './Action.scss';
+
+export interface ActionProps {
+  isReversed: boolean;
+  actionState: 'none' | 'block' | 'attack';
+  attackValue: number;
+}
+
+const Action: FunctionComponent<ActionProps> = ({
+  isReversed,
+  actionState,
+  attackValue,
+}) => {
+  if (actionState === 'none') {
+    return null;
+  }
+  const isAttack = actionState === 'attack';
+  const iconSource = isAttack ? SwordIcon : ShieldIcon;
+  return (
+    <div>
+      {isAttack && <p className="attackValue">{attackValue}</p>}
+      <img
+        src={iconSource}
+        className={`${isReversed ? 'iconReversed' : undefined}`}
+      />
+    </div>
+  );
+};
+
+export default Action;
