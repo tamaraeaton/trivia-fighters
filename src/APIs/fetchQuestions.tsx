b89527596acafc1e2b91591da4be163c8997c453
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-09-13 12:36:58 -0700

    RAP396: - cleaned up fetchQuestions

--- src/APIs/fetchQuestions.tsx
+++ src/APIs/fetchQuestions.tsx
@@ -22,21 +22,25 @@ export const fetchQuestionsPerDifficulty = async (
     // Removing special characters from the JSON
     const json = await res.json();
     const jsonString = await JSON.stringify(json)
-      // .replace(/&quot;/g, '\\"')
-      // .replace(/&#039;/g, "\\'")
-      .replace(/&rsquo;/g, '\\’')
+      .replace(/&quot;/g, '\\"')
+      .replace(/&rsquo;/g, '’')
+      .replace(/&#039;/g, '’')
       .replace(/&amp;/gi, '&')
       .replace(/&lrm;/g, '')
       .replace(/&eacute;/g, 'é')
       .replace(/&oacute;/g, 'ó')
+      .replace(/&ocirc;/g, 'ô')
       .replace(/&sup2;/g, '²')
       .replace(/&Nu;/g, 'Ν')
       .replace(/&Sigma;/g, 'Σ')
       .replace(/&Pi;/g, 'Π')
       .replace(/&Omicron;/g, 'Ο')
-      .replace(/&ocirc;/g, 'ô')
-      .replace(/&aacute;/, 'á');
+      .replace(/&aacute;/, 'á')
+      .replace(/&Uuml;/g, 'Ü')
+      .replace(/&ndash;/g, '–')
+      .replace(/&euml;/g, 'ë');
     const formattedJSON = decodeURI(jsonString);
+    // this console log is here for development test purposes
     console.log(JSON.parse(jsonString).results[0].correct_answer);
     return JSON.parse(formattedJSON);
   } catch (error) {
