0530d7fcf2b440070262d1ec87f53d2171ad03d9
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-09-19 08:28:34 -0700

    RAP396: - refactored two opponent hooks to use the const file

--- src/const/Opponents.ts
+++ src/const/Opponents.ts
@@ -3,6 +3,8 @@ export interface OpponentInterface {
   displayName: string;
   image: 'barbarian-bunny' | 'dragon-seth' | 'fox-knight' | 'wizard-pig';
   maxHealth: number;
+  attackMultiplier: number;
+  attackBase: number;
 }
 
 export const OPPONENTS: { [name: string]: OpponentInterface } = {
@@ -11,18 +13,24 @@ export const OPPONENTS: { [name: string]: OpponentInterface } = {
     displayName: 'Wizard Pig',
     image: 'wizard-pig',
     maxHealth: 100,
+    attackMultiplier: 1,
+    attackBase: 1,
   },
   medium: {
     difficulty: 'Medium',
     displayName: 'Barbarian Bunny',
     image: 'barbarian-bunny',
     maxHealth: 150,
+    attackMultiplier: 2,
+    attackBase: 5,
   },
   seth: {
     difficulty: 'Seth',
     displayName: 'Dragon Seth',
     image: 'dragon-seth',
     maxHealth: 200,
+    attackMultiplier: 3,
+    attackBase: 10,
   },
 };
 
